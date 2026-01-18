a195db5ef819e72911838f7e5b64c6a5ace49634
Joseph Wilfried Ewondjo <ewondjojoseph@gmail.com>
2026-01-19 00:28:13 +0100

    integration

--- frontend-flcs/src/app/layout/dashboard-layout/dashboard-layout.ts
+++ frontend-flcs/src/app/layout/dashboard-layout/dashboard-layout.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard-layout',
   imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './dashboard-layout.html',
   styleUrls: ['./dashboard-layout.scss']
 })
-export class DashboardLayout {}
+export class DashboardLayout {
+
+    constructor(private router: Router) {}
+
+   logout() {
+    // Ici tu peux aussi vider le localStorage/sessionStorage si tu gères un token
+    console.log('Déconnexion');
+    this.router.navigate(['/login']);
+  }
+}
